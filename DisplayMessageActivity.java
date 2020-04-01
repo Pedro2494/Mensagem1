973fa787cd9e6fdd25e071d0f05f27e12d478552
Pedro2494 <antunes-pedro@hotmail.com>
2020-04-01 14:45:35 +0100

    adicionado o codigo necessario ao botao "send" e criado outro layout para a visualizaçao das mensagens

---
+++ DisplayMessageActivity.java
@@ -0,0 +1,14 @@
+package pt.ipg.mensagem2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
